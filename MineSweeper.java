04cc31b26212c51768544c31efdedeb387b6a3c9
Nicholas-Schatteman <nicholasschatteman@gmail.com>
2025-04-08 11:48:47 -0400

    MineSweeper.java did not add

--- MineSweeper.java
+++ MineSweeper.java
@@ -1,134 +1,31 @@
-import java.util.Scanner;
+import java.awt.Toolkit;
+import javax.swing.JFrame;
 
-public class MineSweeper{
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        boolean isLost = false;
-        int rowInput;
-        int columnInput;
-        int bombsInput;
+import java.io.File;
+import javax.imageio.ImageIO;
 
-        Object pointInput[];
-        boolean toFlag;
+import java.io.IOException;
 
-        rowInput = positiveUserInput(input, "the number of rows.");
-        columnInput = positiveUserInput(input, "the number of columns.");
-        bombsInput = positiveUserInput(input, "the number of bombs on the board.");
+import java.awt.image.BufferedImage;
 
-        MineField board = new MineField(rowInput, columnInput, bombsInput);
-        board.printConsole();
+public class MineSweeper {
+    public static void main(String[]args){
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        JFrame gameWindow = new JFrame("Mine Sweeper");
+        GraphicsRendering render = new GraphicsRendering();
 
-        Scanner scan = new Scanner(System.in);
-
-        while (!board.isWon() && !isLost){
-            pointInput = positionOperationInput(scan,board.rowLength(), board.columnLength());
-            rowInput = (int)pointInput[0];
-            columnInput = (int)pointInput[1];
-            toFlag = (boolean)pointInput[2];
-
-            if (toFlag){
-                if (!board.isSeen(rowInput, columnInput)){
-                    board.addFlag(rowInput, columnInput);
-                }
-            }
-            else{
-                if (!board.isFlaged(rowInput, columnInput)){
-                    isLost = board.check(rowInput, columnInput);
-                }
-            }
-
-            board.printConsole();
+        gameWindow.setSize(tk.getScreenSize());
+        try{
+            BufferedImage icon = ImageIO.read(new File("Images\\Icon.png"));
+            gameWindow.setIconImage(icon);
+        }catch (IOException e){
+            System.out.println(e.getMessage());
         }
 
-        if (board.isWon()){
-            System.out.println("-O-O\n\\___/\n");
-        }
-        else{
-            System.out.println(" x x\n ___\n/   \\");
-        }
-    }
+        gameWindow.add(render);
 
-    public static int positiveUserInput(Scanner input, String request){
-        int inputVal = 0;
-        
-        System.out.println("Please enter " + request);
-        while (true){
-            if (input.hasNextInt()){
-                inputVal = input.nextInt();
+        gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-                //If else whether input is positive
-                if (inputVal > 0){
-                    return inputVal;
-                }
-                else{
-                    System.out.println("Input must be greater than 0.");
-                }
-            }
-            else{
-                input.next();
-                System.out.println("Input must be an integer");
-            }
-        }
-
-    }
-
-    public static Object[] positionOperationInput(Scanner input, int xLength, int yLength){
-        int row = 0;
-        int column = 0;
-        boolean isFlag = false;
-
-        String message = "";
-        boolean isValid = true;
-        int comma;
-
-        //While rows, columns, or format are not valid
-        while (!isValid || !(0 < row && row <= xLength)){
-            isValid = true;
-
-            message = input.nextLine();
-            if (message.isBlank()) isValid = false;
-
-            comma = message.indexOf(',');
-            if (comma == -1) isValid = false;
-
-
-            row = findNumber(message, 0, comma);
-            if (!(0 < row && row <= xLength)) isValid = false;
-
-            column = findNumber(message, comma + 1, message.length());
-            if (!(0 < column && column <= yLength)) isValid = false;
-
-
-            if (isValid && message.charAt(message.length() - 1) == 'f' || message.charAt(message.length() - 1) == 'F'){
-                isFlag = true;
-            }
-        }
-
-        return new Object[]{row,column,isFlag};
-    }
-
-    //Finds the first number between start (inclusive) and end (exclusive)
-    public static int findNumber(String toFind, int start, int end){
-        String number = "";
-        boolean previouslyNumber = false;
-        for (int index = start; index < end; index++){
-            //If the index is a digit
-            if ('0' <= toFind.charAt(index) && toFind.charAt(index) <= '9'){
-                previouslyNumber = true;
-                number += toFind.charAt(index);
-            }
-            else if (previouslyNumber){
-                index = end;
-            }
-        }
-
-        if (number.isEmpty()){
-            return -1;
-        }
-        else{
-            return Integer.parseInt(number);
-        }
-
-        
+        gameWindow.setVisible(true);
     }
 }
